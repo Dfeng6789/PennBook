fbf2e103833e530f825ae6480001e8ae2cdd1626
David Feng <70027279+Dfeng6789@users.noreply.github.com>
2022-01-06 21:03:02 -0700

    Add files via upload

---
+++ newsfeed/src/main/java/edu/upenn/cis/nets212/config/Config.java
@@ -0,0 +1,16 @@
+package edu.upenn.cis.nets212.config;
+
+/**
+ * 
+ * @author zives
+ *
+ */
+public class Config {
+	public static int DYNAMODB_LOCAL_PORT = 8000;
+
+	public static String DYNAMODB_URL = "https://dynamodb.us-east-1.amazonaws.com";
+
+	public static Boolean LOCAL_DB = false;
+
+	public static int PARTITIONS = 5;
+}
